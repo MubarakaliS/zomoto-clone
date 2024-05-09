49721c36ae94562e617366c2d725096cea5a73d2
MubarakaliS <19bcamubarak777@gmail.com>
2024-05-09 20:25:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+function App() {
+  return (
+  <>
+  <Home/>
+  </>
+  );
+}
+
+export default App;
